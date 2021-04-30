c65155b85749663404a95af88d08768d94094a6e
Dang Kien <kd049@hdm-stuttgart.de>
2021-04-30 17:14:51 +0200

    Stuff

--- Testdatei.py
+++ Testdatei.py
@@ -7,32 +7,40 @@ client = discord.Client()
 
 
 @client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.guild.id != 837676563583336458:
-        print(f"From somewhere far away")
-        return
-
-    if message.channel.id != 837676563583336461:
-        print("Wrong channel, bro")
-        return
-
-    print(f"[new message] {message.guild.name} > {message.channel.name} > {message.author}")
-
-    if message.content.startswith('!hello'):
-        msg = "Hello {0.author.mention}".format(message)
-        await message.channel.send(msg)
-
-@client.event
 async def on_ready():
     print('Logged in as')
     print('\t', client.user.name)
     print('\t', client.user.id)
     print('---------------')
 
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    # Info im Terminal wenn jemand eine Nachricht schreibt
+    print(f"[new message] {message.guild.name} > {message.channel.name} > {message.author}")
+
+    if message.guild.id != 837676563583336458:
+        print(f"Wrong server, bro")
+        return
+
+    if message.channel.id != 837676563583336461:
+        print("Wrong channel, bro")
+        return
+
+    # Hier kommen wir an wenn es der richtige channel und server ist
+    # Spezifischer Code ab hier
+
+    if message.content.startswith('!hello'):
+        msg = "Hello {0.author.mention}".format(message)
+        await message.channel.send(msg)
+
+
+# def save_locally(file):
+
+
+# Do not touch this. Starts the bot
 client.run(TOKEN)
-
-
