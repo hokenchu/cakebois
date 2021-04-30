0c59320b4863fc499208746fd722325a89d7f617
Dang Kien <kd049@hdm-stuttgart.de>
2021-04-30 19:13:55 +0200

    Added cakeboi_sheets.py > Change cells automatically in google sheets

---
+++ cakeboi_sheets.py
@@ -0,0 +1,66 @@
+# google spreadsheets
+import gspread
+
+# authentication packages, for "Login"
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Subroutine to find position of target cell.
+def get_position(worksheet):
+    """
+    Subroutine to find position of target cell.
+
+    Target cell is the cell containing today's date.
+    Returns row and col number.
+    """
+    from datetime import date
+
+    # string format time
+    # % :: start of placeholder
+    # d :: day (numeric)
+
+    # a :: weekday (abbr)
+    # A :: weekday
+    # b :: month
+
+    cell_of_today = date.today().strftime("%a-%d-%b")
+    return worksheet.find(cell_of_today).row, worksheet.find(cell_of_today).col
+
+
+def verify():
+    scope = ["https://spreadsheets.google.com/feeds",
+             'https://www.googleapis.com/auth/spreadsheets',
+             "https://www.googleapis.com/auth/drive.file",
+             "https://www.googleapis.com/auth/drive"]
+
+    return ServiceAccountCredentials.from_json_keyfile_name("config/creds.json", scope)
+
+
+# main routine
+if __name__ == '__main__':
+
+    user = gspread.authorize(verify())
+
+    # Worksheet (Reference)
+    worksheet = user.open("Discord Cakeboi").sheet1  # Open the spreadsheet
+
+    # get position of "today cell"
+    (row, col) = get_position(worksheet)
+
+
+    # TODO
+    img_list = ["Image1", "Image2", "Image3", "Image4", "Image5", "Image6", ]  # ... bis 9 Links
+
+    # insert guild name into left cell of 'date'
+    # TODO
+    worksheet.update_cell(row, col - 1, "Guild_Name_plcd")
+
+    # inserts certain link
+    # EXAMPLE
+    img_link = "http://pullSomePicsOutaMyAss.com"  # Placeholder
+    mein_string = f'=IMAGE("{img_link}")'
+    worksheet.update_cell(row, col + 1, mein_string)
+
+    x = 1
+    for img in img_list:
+        worksheet.update_cell(row, col + x, f"{img}")
+        x = x + 1
