36520b7fb5eb7f4f531c147ed9cf909c13f20a10
Dang Kien <kd049@hdm-stuttgart.de>
2021-05-01 00:57:39 +0200

    Added simple file upload

--- utility/drive.py
+++ utility/drive.py
@@ -1,19 +1,24 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
-gauth = GoogleAuth()
-# gauth.LocalWebserverAuth()
-gauth.LoadCredentialsFile(r"../config/login.txt")
-if gauth.credentials is None:
-    # Authenticate if they're not there
-    gauth.LocalWebserverAuth()
-elif gauth.access_token_expired:
-    # Refresh them if expired
-    gauth.Refresh()
-else:
-    # Initialize the saved creds
-    gauth.Authorize()
-# Save the current credentials to a file
-gauth.SaveCredentialsFile(r"../config/mycreds.txt")
+TMP_FOLDER = r"../tmp"
 
-drive = GoogleDrive(gauth)
+if __name__ == '__main__':
+    gauth = GoogleAuth()
+    # gauth.LocalWebserverAuth()
+    gauth.LoadCredentialsFile(r"../config/credentials.txt")
+    if gauth.credentials is None:
+        # Authenticate if they're not there
+        gauth.LocalWebserverAuth()
+    elif gauth.access_token_expired:
+        # Refresh them if expired
+        gauth.Refresh()
+    else:
+        # Initialize the saved creds
+        gauth.Authorize()
+
+    drive = GoogleDrive(gauth)
+    file = drive.CreateFile()
+    file.SetContentFile('../waifus/waifu.png')
+    file['title'] = "testFile"
+    file.Upload()
