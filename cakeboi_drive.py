ebe75c4717161f30ae7046263734c29a76486385
Dang Kien <kd049@hdm-stuttgart.de>
2021-04-30 22:13:41 +0200

    Added timed purge task and basic Save_to_file() in discord

--- cakeboi_drive.py
+++ cakeboi_drive.py
@@ -1,18 +1,4 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START drive_quickstart]
+from __future__ import print_function
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -20,40 +6,40 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
 # If modifying these scopes, delete the file token.json.
-from oauth2client.service_account import ServiceAccountCredentials
+SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
 
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
-
-
-def log_in():
-    scope = ["https://spreadsheets.google.com/feeds",
-             'https://www.googleapis.com/auth/spreadsheets',
-             "https://www.googleapis.com/auth/drive.file",
-             "https://www.googleapis.com/auth/drive"]
-
-    return ServiceAccountCredentials.from_json_keyfile_name("config/credentials.json", scope)
-
+# The ID of a sample document.
+DOCUMENT_ID = '195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE'
 
 def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
+    """Shows basic usage of the Docs API.
+    Prints the title of a sample document.
     """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('resources/token.json'):
+        creds = Credentials.from_authorized_user_file('resources/token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                './resources/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('resources/token.json', 'w') as token:
+            token.write(creds.to_json())
 
-    creds = log_in()
-    service = build('drive', 'v3', credentials=creds)
+    service = build('docs', 'v1', credentials=creds)
 
-    # Call the Drive v3 API
-    results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
+    # Retrieve the documents contents from the Docs service.
+    document = service.documents().get(documentId=DOCUMENT_ID).execute()
 
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
+    print('The title of the document is: {}'.format(document.get('title')))
+
 
 if __name__ == '__main__':
     main()
-
--- main.py
+++ main.py
@@ -1,46 +1,89 @@
 # Work with Python 3.6
 import discord
-from utility.playground import my_function as do_stuff
+from discord.ext import tasks
 
 TOKEN = 'ODM3Njc1OTQ4MzQ5ODQ5NjQx.YIwAhQ.Wvc3LwhUfhkS4Qvk_cp-8H5x8iI'
 
 client = discord.Client()
+bot_channel = None;
 
 
 @client.event
 async def on_ready():
     print('Logged in as')
-    print('\t', client.user.name)
-    print('\t', client.user.id)
+    print('>', f"{client.user.name} (id:{client.user.id})")
+    global bot_channel
+    bot_channel = client.get_channel(837676563583336461)
+    print('>', f"[{bot_channel.guild}] \"{bot_channel.name}\" (id:{bot_channel.id})")
+
+    print("Initiating Purging Task")
+    loop_purge.start(3600) # inactive time in seconds
+
     print('---------------')
+
+
+@tasks.loop(seconds=3600) # frequency of checks
+async def loop_purge(inactivity_time):
+    history = await bot_channel.history(limit=100).flatten()
+    if len(history) == 0:
+        return
+
+    from datetime import datetime
+    import pytz
+
+    last = history[0].created_at.timestamp()
+    now = datetime.today().astimezone(pytz.timezone('UTC')).timestamp() - 7200
+    print("[Log]", f"Last message was {now - last} seconds ago")
+    if now - last > inactivity_time:
+        await bot_channel.purge()
+        print("[Log]", f"Purged {bot_channel}")
 
 
 @client.event
 async def on_message(message):
+    global bot_channel
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
+    if type(message.channel) == discord.DMChannel:
+        print("Why are you whispering, bro")
+        print(f"[new message] direct message > {message.author}")
+        return
+
     # Info im Terminal wenn jemand eine Nachricht schreibt
-    print(f"[new message] {message.guild.name} > {message.channel.name} > {message.author}")
+    print(f"[new message] {message.guild.name} > {message.channel.name or 'DM'} > {message.author}")
 
-    if message.guild.id != 837676563583336458:
-        print(f"Wrong server, bro")
+    if message.guild.id != 837676563583336458 or message.channel.id != 837676563583336461:
+        print(f"[Info] Wrong channel, bro")
         return
 
-    if message.channel.id != 837676563583336461:
-        print("Wrong channel, bro")
-        return
+    if message.content.startswith('!purge'):
+        await message.channel.purge(limit=100)
 
-    # Hier kommen wir an wenn es der richtige channel und server ist
-    # Spezifischer Code ab hier
+    if message.content.startswith('!upload'):
+        latest_messages = await bot_channel.history(
+            limit=12).flatten()
+        # Increase limit to compensate for user error
 
-    if message.content.startswith('!hello'):
-        msg = "Hello {0.author.mention}".format(message)
-        await message.channel.send(msg)
+        print(latest_messages)
+        # TODO filter older messages
 
-    if message.content.startswith('!test'):
-        await do_stuff(message)
+        for msg in latest_messages:
+            if msg.content.startswith("Guild:"):  # TODO Voraussetzung
+                print(">> You fought against ", msg.content.replace('Guild', '').replace(':', ""))  # TODO name filtern
+
+            if msg.content in ["win", "victory", "defeat", "lose"]:
+                print(">> Lul, you ", msg.content)
+
+            if len(msg.attachments) > 0:
+                print(">> ", 'You sent some pictures')
+
+
+    if message.content.startswith('!save'):
+        for attachment in message.attachments:
+            print(attachment.filename)
+            await attachment.save(f"./resources/{attachment.filename}")
 
 
 # Do not touch this. Starts the bot
