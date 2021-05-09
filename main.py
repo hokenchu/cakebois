23395623351423a439ebf0b73a3254c0515101c7
Dang Kien <kd049@hdm-stuttgart.de>
2021-05-10 00:02:21 +0200

    cleanup

--- main.py
+++ main.py
@@ -2,12 +2,8 @@
 
 from cakeboi.bot import cakeboi
 from cakeboi import validate_users
+# from cakeboi.util import gdrive
 if __name__ == '__main__':
     validate_users.run()
-    from cakeboi.util import gdrive
 
-    gdrive.create_token()
-
-    # u = gdrive.DriveUser(name="Realm")
-
-    # cakeboi.run()
+    cakeboi.run()
